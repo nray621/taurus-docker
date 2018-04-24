1808e0e324ceb4847b6145ee89cc157951e1918b
Noah Ray <noahray621@gmail.com>
2018-04-24 13:28:14 -0400

    working server logging request

--- server.py
+++ server.py
@@ -1,52 +1,45 @@
-import http.server
-import socketserver
-import subprocess
-import json
-import urllib.parse
+# import http.server
+# import socketserver
+# import subprocess
+# import json
+# import urllib.parse
 
-PORT = 8000
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
 
-Handler = http.server.SimpleHTTPRequestHandler
-
-# httpd = socketserver.TCPServer(("", PORT), Handler)
-
-# print("serving at port", PORT)
-# httpd.serve_forever()
-
-# subprocess.call('bzt test.yml', shell=True)
-
-class S(Handler):
-    def _set_headers(self):
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
 
     def do_GET(self):
-        print('in do_GET')
-        self._set_headers()
-        parsed_path = urllib.parse(self.path)
-        request_id = parsed_path.path
-        response = subprocess.check_output(["python", request_id])
-        self.wfile.write(json.dumps(response))
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
 
     def do_POST(self):
-        self._set_headers()
-        parsed_path = urllib.parse(self.path)
-        request_id = parsed_path.path
-        response = subprocess.check_output(["python", request_id])
-        self.wfile.write(json.dumps(response))
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                str(self.path), str(self.headers), post_data.decode('utf-8'))
 
-    def do_HEAD(self):
-        self._set_headers()
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
-def run(server_class=http.server.HTTPServer, handler_class=S, port=8000):
-    print('in run')
+def run(server_class=HTTPServer, handler_class=S, port=8000):
+    logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print('Starting httpd...')
-    httpd.serve_forever()
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     from sys import argv
 
     if len(argv) == 2:
